494587e1e79156d865231c7f2078dc6dff5d5f1c
Amitav Roy <reachme@amitavroy.com>
2014-07-16 15:12:56 +0530

    Finalizing.

--- workbench/amitavroy/mailing/src/routes.php
+++ workbench/amitavroy/mailing/src/routes.php
@@ -1,7 +1,7 @@
 <?php
 Route::get('sendmail', function  ()
     {
-        $MailTracker = new MailTracker;
+        /*$MailTracker = new MailTracker;
 
         $mail_to = 'reachme@amitavroy.com';
         $mail_from = 'amitav.roy@focalworks.in';
@@ -9,7 +9,7 @@ Route::get('sendmail', function  ()
         $mail_body = '<p>This is some <strong>bold</strong> text.';
         $mail_to_name = 'Amitav Roy Offiec';
         $mail_from_name = 'My site admin';
-//        $MailTracker->sendMail($mail_to, $mail_from, $mail_subject, $mail_body, $mail_to_name, $mail_from_name);
+        $MailTracker->sendMail($mail_to, $mail_from, $mail_subject, $mail_body, $mail_to_name, $mail_from_name);*/
     });
 
 Route::group(array('before' => 'checkAuth'), function() {
