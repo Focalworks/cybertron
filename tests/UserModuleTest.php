f3905afafd912bdc0b3e7a6d3bfb9693bae01e3b
Amitav Roy <reachme@amitavroy.com>
2015-07-05 20:36:42 +0530

    Added tests for change password functionality.

--- tests/UserModuleTest.php
+++ tests/UserModuleTest.php
@@ -3,6 +3,23 @@
 class UserModuleTest extends TestCase
 {
     /**
+     * User object required for auth based urls.
+     *
+     * @var user object
+     */
+    protected $user;
+
+    /**
+     * Setting values on init of the class.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $user = App\User::find(1);
+        $this->user = $user;
+    }
+
+    /**
      * Basic test to check if login screen is available.
      *
      * @return void
@@ -54,6 +71,10 @@ class UserModuleTest extends TestCase
              ->see('Something went wrong with the username and / or password');
     }
 
+    /**
+     * Check login is working with correct credentials
+     * @return void
+     */
     public function testWithCorrectCredentials()
     {
         $this->visit('/user')
@@ -62,5 +83,76 @@ class UserModuleTest extends TestCase
              ->press('Login')
              ->see('You have logged in successfully')
              ->seePageIs('/user/dashboard');
+    }
+
+    /**
+     * Checking the change password page is working.
+     *
+     * @return void
+     */
+    public function testChangePasswordPageIsOk()
+    {
+        $this->actingAs($this->user)
+            ->visit('/user/change-password')
+            ->see('Change your password');
+    }
+
+    /**
+     * Check error when no input is provided.
+     *
+     * @return void
+     */
+    public function testCheckPasswordWithEmptyFields()
+    {
+        $this->actingAs($this->user)
+            ->visit('/user/change-password')
+            ->press('Save')
+            ->see('Current password is wrong.');
+    }
+
+    /**
+     * Check error when the password is wrong.
+     *
+     * @return void
+     */
+    public function testCheckWhenPasswordIsWrong()
+    {
+        $this->actingAs($this->user)
+            ->visit('/user/change-password')
+            ->type(time(), 'current_password')
+            ->press('Save')
+            ->see('Current password is wrong.');
+    }
+
+    /**
+     * Check error when the two passwords are not same.
+     *
+     * @return void
+     */
+    public function testCheckWhenPasswordsDoNotMatch()
+    {
+        $this->actingAs($this->user)
+            ->visit('/user/change-password')
+            ->type('pass', 'current_password')
+            ->type('pass', 'new_password')
+            ->type('pass1', 'confirm_new_password')
+            ->press('Save')
+            ->see('The two passwords are not matching.');
+    }
+
+    /**
+     * Check change password functionality with correct data.
+     *
+     * @return void
+     */
+    public function testCheckWhenChangePasswordDataIsCorrect()
+    {
+        $this->actingAs($this->user)
+            ->visit('/user/change-password')
+            ->type('pass', 'current_password')
+            ->type('pass', 'new_password')
+            ->type('pass', 'confirm_new_password')
+            ->press('Save')
+            ->see('Password changed.');
     }
 }
